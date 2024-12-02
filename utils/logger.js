2c4f99ac12796b0415065c106dc20f579ec533f4
SGCMarketing <SGCMarketing>
2024-12-02 21:57:10 +0000

    IP

--- utils/logger.js
+++ utils/logger.js
@@ -91,7 +91,7 @@ const logger = () => {
     const request = (req) => {
         msgStack.push({ type: 'request', msg: req?.method })
         if (!processing) processStack()
-        _printLogToConsole(`${newLine}${loggerInstance.penStart.brightCyan}\nREQ: ${req?.method} ${req?.url}\n${newLine}\n${loggerInstance.penEnd}`)
+        _printLogToConsole(`${newLine}${loggerInstance.penStart.brightCyan}\nREQ: ${req?.method} ${req?.url}\n${newLine}${loggerInstance.penEnd}\n`)
     }
 
     const info = (msg) => {
